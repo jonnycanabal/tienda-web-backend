2af72f559bc0403dc6d3b5ac9a5365118b24b91c
jonnycanabal <jonny.canabal@gmail.com>
2023-12-03 21:54:46 -0500

    Entity ItemCarrito - modificar(CarritoController)

--- src/main/java/com/tienda/web/app/controllers/ItemCarritoController.java
+++ src/main/java/com/tienda/web/app/controllers/ItemCarritoController.java
@@ -1,8 +1,6 @@
 package com.tienda.web.app.controllers;
 
-
 import java.util.Optional;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,25 +14,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tienda.web.app.models.entity.Carrito;
-import com.tienda.web.app.services.CarritoService;
+import com.tienda.web.app.models.entity.ItemCarrito;
+import com.tienda.web.app.services.ItemCarritoService;
 
 @RestController
-@RequestMapping("/carrito")
-public class CarritoController {
+@RequestMapping("/itemcarrito")
+public class ItemCarritoController {
 	
 	@Autowired
-	private CarritoService service; 
-
+	private ItemCarritoService service;
+	
 	@GetMapping
 	public ResponseEntity<?> listar(){
-		return ResponseEntity.ok().body(service.finAll());
+		return ResponseEntity.ok().build();
 	}
 	
 	@GetMapping("/ver/{id}")
 	public ResponseEntity<?> ver(@PathVariable Long id){
 		
-		Optional<Carrito> o = service.finById(id);
+		Optional<ItemCarrito> o = service.finById(id);
 		
 		if(o.isEmpty()) {
 			return ResponseEntity.notFound().build();
@@ -43,48 +41,37 @@ public class CarritoController {
 		return ResponseEntity.ok().body(o.get());
 	}
 	
-	@PostMapping("/crear")
-	public ResponseEntity<?> crear(@RequestBody Carrito carrito){
+	@PutMapping("/crear")
+	public ResponseEntity<?> crear(@RequestBody ItemCarrito itemCarrito){
 		
-		if(carrito.getUsuario() == null || carrito.getProductos().isEmpty()) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No hay usuario y/o productos");
-		}
+		ItemCarrito itemCarritoDb= service.save(itemCarrito);
 		
-		carrito.getItems().forEach(item -> item.setCarrito(carrito));
-		
-		Carrito carritoNuevo = service.save(carrito);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(carritoNuevo);
+		return ResponseEntity.status(HttpStatus.CREATED).body(itemCarritoDb);
 	}
 	
-	@PutMapping("/editar/{id}")
-	public ResponseEntity<?> editar(@RequestBody Carrito carrito, @PathVariable Long id){
+	@PostMapping("/editar/{id}")
+	public ResponseEntity<?> editar(@RequestBody ItemCarrito itemCarrito, @PathVariable Long id){
 		
-		Optional<Carrito> o = service.finById(id);
+		Optional<ItemCarrito> o = service.finById(id);
 		
 		if(o.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
 		
-		Carrito carritodb = o.get();
+		ItemCarrito itemCarritoDb = o.get();
 		
-		carritodb.setProductos(carrito.getProductos());
+		itemCarritoDb.setCarrito(itemCarrito.getCarrito());
+		itemCarritoDb.setProducto(itemCarrito.getProducto());
+		itemCarritoDb.setCantidad(itemCarrito.getCantidad());
 		
-		if(carrito.getUsuario() != null) {
-			carritodb.setUsuario(carrito.getUsuario());
-		}else {
-			carritodb.setUsuario(null);
-		}
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(carritodb));
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(itemCarrito));
 	}
 	
-	@DeleteMapping("eliminar/{id}")
+	@DeleteMapping("/eliminar/{id}")
 	public ResponseEntity<?> eliminar(@PathVariable Long id){
-		
 		service.deleteById(id);
 		
 		return ResponseEntity.noContent().build();
 	}
-	
+
 }
